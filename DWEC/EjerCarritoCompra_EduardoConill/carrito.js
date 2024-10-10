ae897ef166085aa6215485194ab97ac81bcbf8f1
COMPU-S027\daw2 <eduardoconillgomez@gmail.com>
2024-10-10 18:34:35 +0200

    10/10/24  Clase DWEC

---
+++ DWEC/EjerCarritoCompra_EduardoConill/carrito.js
@@ -0,0 +1,13 @@
+class Carrito {
+    constructor(productos) {
+    }
+    actualizarUnidades(sku, unidades) {
+    }
+    obtenerInformacionProducto(sku) {
+ 
+    }
+    obtenerCarrito() {
+    
+    }
+    }
+   
